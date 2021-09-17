cc14f2432f1996f7da56af9a3aa4335dd7f82c2a
michael727813 <michael727813@yahoo.com>
2021-09-16 22:20:45 -0400

    Menu
    Update CSS and JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
 console.log("Hello World")
+function menuToggle() {
+}
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+  }
+  function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    x.className += ' responsive';
+  }
+  function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
