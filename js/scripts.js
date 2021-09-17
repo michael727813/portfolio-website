06ed622468a92c7acc6b6a47b5d7d6a6e09f6e4e
michael727813 <michael727813@yahoo.com>
2021-09-17 17:13:16 -0400

    Menu update

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,13 @@ function menuToggle() {
       x.className = 'navtoggle';
     }
   }
+  $('#heading-text').text('How to create a hamburger menu with jQuery');
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
