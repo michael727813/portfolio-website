22d413f887b3c7a50b5033781bda85ea3edeb625
michael727813 <michael727813@yahoo.com>
2021-09-27 16:25:14 -0400

    Fixed error
    Fixed error

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,6 @@
-console.log("Hello World")
-function menuToggle() {
-}
-function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-  }
-  function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    x.className += ' responsive';
-  }
-  function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
-  }
-  $('#heading-text').text('How to create a hamburger menu with jQuery');
+$('#heading-text').text('How to create a hamburger menu with jQuery'); 
 
-function menuToggle() {
+ function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
     x.className += ' responsive';
